6c57507d45987c845d8401afa42a1e13becef846
Allan Reyes <allan_bassail@hotmail.com>
2025-01-01 16:19:39 -0800

    Spawn an ant on each ant hill when the game starts

--- ants_engine/src/entities.rs
+++ ants_engine/src/entities.rs
@@ -2,18 +2,26 @@ use std::any::type_name;
 use uuid::Uuid;
 
 pub trait Entity {
+    fn name(&self) -> &str {
+        type_name::<Self>().rsplit("::").next().unwrap()
+    }
+
     fn id(&self) -> &str {
         "Entity"
     }
+
     fn player(&self) -> usize {
         usize::MAX
     }
+
     fn is_alive(&self) -> bool {
         false
     }
+
     fn set_alive(&mut self, _value: bool) {}
-    fn name(&self) -> &str {
-        type_name::<Self>().rsplit("::").next().unwrap()
+
+    fn on_ant_hill(&self) -> &Option<Box<dyn Entity>> {
+        &None
     }
 }
 
@@ -21,7 +29,20 @@ pub struct Ant {
     id: String,
     player: usize,
     is_alive: bool,
+    on_ant_hill: Option<Box<dyn Entity>>,
 }
+
+impl Ant {
+    pub fn from_ant_hill(player: usize, ant_hill: Box<dyn Entity>) -> Self {
+        Ant {
+            id: Uuid::new_v4().to_string(),
+            player,
+            is_alive: true,
+            on_ant_hill: Some(ant_hill),
+        }
+    }
+}
+
 impl Entity for Ant {
     fn id(&self) -> &str {
         &self.id
@@ -38,6 +59,10 @@ impl Entity for Ant {
     fn set_alive(&mut self, value: bool) {
         self.is_alive = value;
     }
+
+    fn on_ant_hill(&self) -> &Option<Box<dyn Entity>> {
+        &self.on_ant_hill
+    }
 }
 
 pub struct Food;
@@ -46,6 +71,13 @@ impl Entity for Food {}
 pub struct Hill {
     player: usize,
 }
+
+impl Hill {
+    pub fn new(player: usize) -> Self {
+        Hill { player }
+    }
+}
+
 impl Entity for Hill {
     fn player(&self) -> usize {
         self.player
@@ -66,6 +98,7 @@ pub fn from_char(value: char) -> Option<Box<dyn Entity>> {
             // Convert char to digit for player number where 'a' is 0 and so on
             player: value as usize - 'a' as usize,
             is_alive: true,
+            on_ant_hill: None,
         })),
         '*' => Some(Box::new(Food)),
         // Max 10 players
--- ants_engine/src/game.rs
+++ ants_engine/src/game.rs
@@ -1,3 +1,5 @@
+use crate::entities::Ant;
+use crate::entities::Hill;
 use crate::map::Map;
 use std::fs;
 
@@ -26,6 +28,25 @@ impl Game {
     /// Starts the game.
     pub fn start(&mut self) {
         self.map = Map::parse(&self.map_contents);
+
+        // Spawn 1 ant per ant hill
+        let ant_hills = self
+            .map
+            .ant_hills()
+            .into_iter()
+            .map(|(hill, row, col)| (hill.player(), row, col))
+            .collect();
+        spawn_ants(&mut self.map, ant_hills);
+    }
+}
+
+fn spawn_ants(map: &mut Map, ant_hills: Vec<(usize, usize, usize)>) {
+    for (player, row, col) in ant_hills {
+        map.set(
+            row,
+            col,
+            Box::new(Ant::from_ant_hill(player, Box::new(Hill::new(player)))),
+        );
     }
 }
 
@@ -45,5 +66,27 @@ mod tests {
 
         // The example map has water at (0, 0)
         assert_eq!(game.map.get(0, 0).as_ref().unwrap().name(), "Water");
+    }
+
+    #[test]
+    fn when_starting_a_game_ants_are_spawned_on_ant_hills() {
+        let map_file = Path::new(env!("CARGO_MANIFEST_DIR")).join("tests/test_data/example.map");
+        let mut game = Game::new(map_file.to_str().unwrap());
+
+        game.start();
+
+        // The example map has 1 ant hill at (0, 1) for player 1
+        let ant = game.map.get(0, 1).as_ref().unwrap();
+        assert_eq!(ant.name(), "Ant");
+        assert_eq!(ant.player(), 1);
+        assert_eq!(ant.is_alive(), true);
+        assert_eq!(ant.on_ant_hill().as_ref().unwrap().player(), 1);
+
+        // The example map has 1 ant hill at (3, 2) for player 0
+        let ant = game.map.get(3, 2).as_ref().unwrap();
+        assert_eq!(ant.name(), "Ant");
+        assert_eq!(ant.player(), 0);
+        assert_eq!(ant.is_alive(), true);
+        assert_eq!(ant.on_ant_hill().as_ref().unwrap().player(), 0);
     }
 }
