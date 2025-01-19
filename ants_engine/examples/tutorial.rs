d8f21a748d00492565b9d3ebff3edaa6493c1342
Allan Reyes <allan_bassail@hotmail.com>
2025-01-18 16:42:27 -0800

    Rename example from `render` -> `tutorial` and add a single game update

--- ants_engine/examples/tutorial.rs
+++ ants_engine/examples/tutorial.rs
@@ -1,10 +1,17 @@
 use ants_engine::Game;
 use std::path::Path;
+use std::thread::sleep;
 
 fn main() {
     let map_file = Path::new(env!("CARGO_MANIFEST_DIR")).join("tests/test_data/example.map");
     let mut game = Game::new(map_file.to_str().unwrap(), 2);
 
     game.start();
+    game.draw();
+
+    // Wait for 1 second for visualization
+    sleep(std::time::Duration::from_secs(1));
+
+    game.update(vec![]);
     game.draw();
 }
